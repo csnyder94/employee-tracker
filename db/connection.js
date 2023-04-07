2d842f74dd964bb63c2ab1d890ce56dda4e7b21c
Christopher Snyder <snyder28139_04@hotmail.com>
2023-04-07 14:37:02 -0400

    added queries

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,15 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2'); //Requiring mysql
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection({ //Creating connection to mysql
   host: 'localhost',
   user: 'root',
   password: 'rootroot',
   database: 'employee_db'
 });
 
-connection.connect(function (err){
+connection.connect(function (err){ //Console log if an error with connection (most times = password issue)
   if(err) throw err
+  console.log(err);
 })
 
-module.exports = connection;
+module.exports = connection; //Exporting connection
